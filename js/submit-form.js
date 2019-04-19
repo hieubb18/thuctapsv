ba8a846c14d0789251a301bb4a9d0632cdb201a3
unknown <hieubb18>
2019-04-19 19:07:41 +0700

    Update submit-form.js

--- js/submit-form.js
+++ js/submit-form.js
@@ -43,3 +43,51 @@ function showErrorMessageReport(error) {
         loading1.classList.add('is-hidden');
     },1000);
 }
+
+
+const editscriptURL = 'https://script.google.com/macros/s/AKfycby_UEQn0LNrCEMAp1z1EoWGqCvfJvn8i9qVX9s2vsgdlZY_Me4/exec';
+const form_edit_SV  = document.forms['submit-to-google-sheet'];
+const loading_edit_SV  = document.querySelector('.js-loading');
+const successMessage_edit_SV  = document.querySelector('.js-success-message-edit');
+const errorMessage_edit_SV  = document.querySelector('.js-error-message-edit');
+const id_getInfo_profile = document.querySelector('.js-showupdate');
+
+var get_form_sv = document.getElementById('getInfo_profile');
+get_form_sv.addEventListener('submit', e => {
+    e.preventDefault();
+    var value_input = $("input[name=MASV]").val().length;
+    if (value_input == 0) {
+        alert('BẠN CẦN NHẬP MÃ SV TRA CỨU THÔNG TIN TRƯỚC KHI THỰC HIỆN THAO TÁC NÀY');
+        return false;
+    }
+    showLoadingIndicator();
+    fetch(editscriptURL, { method: 'POST', body: new FormData(form_edit_SV) })
+        .then(response => showSuccessMessage_edit_SV(response))
+        .catch(error => showRrrorMessage_edit_SV(error));
+})
+
+function showLoadingIndicator() {
+    form.classList.add('is-hidden');
+    loading.classList.remove('is-hidden');
+}
+
+function showSuccessMessage_edit_SV(response) {
+    console.log('Success!', response);
+    setTimeout(() => {
+        successMessage_edit_SV.classList.remove('is-hidden');
+        loading_edit_SV.classList.add('is-hidden');
+        id_getInfo_profile.classList.add('is-hidden');
+
+    },1000);
+    setTimeout(() => {
+        successMessage_edit_SV.classList.add('is-hidden');
+    },5000)
+}
+
+function showRrrorMessage_edit_SV(error) {
+    console.error('Error!', error.message);
+    setTimeout(() => {
+        errorMessage_edit_SV.classList.remove('is-hidden');
+        loading_edit_SV.classList.add('is-hidden');
+    },1000);
+}
