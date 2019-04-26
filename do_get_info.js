79af45b6187d033124069d07c2d85e857d9f67dc
unknown <hieubb18>
2019-04-26 16:23:59 +0700

    Update do_get_info.js

--- do_get_info.js
+++ do_get_info.js
@@ -1,19 +1,19 @@
 // Get the input field
 var input = document.getElementById("inMaSV");
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("btnDoSV").click();
-  }
+input.addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("btnDoSV").click();
+    }
 });
 
 function stepGetSinhVien() {
     var masv = $.trim($("input[name='txtMaSV']").val());
-    if(masv == ''){
+    if (masv == '') {
         alert("VUI LÒNG NHẬP MÃ SINH VIÊN");
         return false;
     }
@@ -21,12 +21,14 @@ function stepGetSinhVien() {
         .then(doGetSinhVien)
         .then(doComplete);
 }
+
 function doLoading() {
     return new Promise(function (resolve, reject) {
         document.querySelector('.js-loading').classList.remove('is-hidden');
         resolve();
     });
 }
+
 function doGetSinhVien() {
     return new Promise(function (resolve, reject) {
         sinhVienGet()
@@ -38,19 +40,20 @@ function doComplete() {
     return new Promise(function (resolve, reject) {
         setTimeout(() => {
             document.querySelector('.js-loading').classList.add('is-hidden');
-        },1000);
+        }, 1000);
         resolve();
     });
 }
 
 function sinhVienGet() {
-	$("#InfoSV").html('');
-	
-    var masv = $.trim($("input[name='txtMaSV']").val()).replace(/ /g,'');
-    
+    $("#InfoSV").html('');
+
+    var masv = $.trim($("input[name='txtMaSV']").val()).replace(/ /g, '');
+
     var worksheets = [
         '', // defaults to first worksheet without id
-        'ouab0ad'];
+        'ouab0ad'
+    ];
 
     worksheets.forEach(function (worksheet) {
         $.googleSheetToJSON('1XjLtjloEkXKTDAsDLanA6Ace9ujINXbw7dClFAn5yeA', worksheet)
@@ -60,12 +63,12 @@ function sinhVienGet() {
                 strText += "<tr> <th>SĐT Giảng Viên</th>  <th>Email GV</th>  <th>Tên GV</th>  <th>Tên SV</th>  <th>Lớp</th> <th>Mã SV</th>  <th>Ngành</th>  <th>Ngày sinh</th>  <th>Email SV</th>  <th>Số ĐT </th>  <th>Môn Học</th>";
                 var count = 0;
                 rows.forEach(function (row) {
-                    var strMaSV = row['masv'].replace(/ /g,'');
+                    var strMaSV = row['masv'].replace(/ /g, '');
                     if (strMaSV == masv) {
                         count++;
                         strText += "<tr>";
                         Object.getOwnPropertyNames(row).forEach(function (name) {
-                            if (name == 'sotc' || name == 'tt' || name == 'mand' || name == 'mamh' || name == 'nhom' || name =='tuan1-ndbaocao')
+                            if (name == 'sotc' || name == 'tt' || name == 'mand' || name == 'mamh' || name == 'nhom' || name == 'tuan1-ndbaocao')
                                 return;
                             if (name == 'sv-email')
                                 $("input[name=EMAIL]").val(row[name]);
@@ -77,20 +80,18 @@ function sinhVienGet() {
                         strText += "</tr>";
                         strText += "<div class='tools-sv'><div class='box-sv'><div class='box-sv-responsive'><button class='btn_' onclick='doupdateInfo();'>Cập nhật thông tin</button></div>";
                         strText += "<div class='box-sv-responsive'><button class='btn_' onclick='doReport();'>Báo cáo</button></div><div class='clear'></div></div></div>";
-                        
-                        
+
+
                     }
                     return;
                 });
-                if (count == 0)
-				{
+                if (count == 0) {
                     $("#InfoSV").html('Không tìm thấy thông tin sinh viên');
-					document.querySelector('.js-showupdate').classList.add('is-hidden');
-				}
-                else {
+                    document.querySelector('.js-showupdate').classList.add('is-hidden');
+                } else {
                     $("#InfoSV").html(strText);
                     $("input[name=MASV]").val(masv);
-					//document.querySelector('.js-showupdate').classList.remove('is-hidden');
+                    //document.querySelector('.js-showupdate').classList.remove('is-hidden');
                 }
             })
             .fail(function (err) {
@@ -114,7 +115,10 @@ form.addEventListener('submit', e => {
         return false;
     }
     showLoadingIndicator();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
         .then(response => showSuccessMessage(response))
         .catch(error => showErrorMessage(error));
 })
@@ -130,11 +134,11 @@ function showSuccessMessage(response) {
         successMessage.classList.remove('is-hidden');
         loading.classList.add('is-hidden');
         id_getInfo_company.classList.add('is-hidden');
-    },1000);
+    }, 1000);
     setTimeout(() => {
         successMessage.classList.add('is-hidden');
 
-    },5000)
+    }, 5000)
 }
 
 function showErrorMessage(error) {
@@ -142,28 +146,68 @@ function showErrorMessage(error) {
     setTimeout(() => {
         errorMessage.classList.remove('is-hidden');
         loading.classList.add('is-hidden');
-    },1000);
+    }, 1000);
 }
 
 
-function doupdateInfo(){
-    
-	document.querySelector('.js-showupdate').classList.remove('is-hidden');
-	document.querySelector('.js-showupadteCompany').classList.remove('is-hidden');
+function doupdateInfo() {
+
+    document.querySelector('.js-showupdate').classList.remove('is-hidden');
+    document.querySelector('.js-showupadteCompany').classList.remove('is-hidden');
     document.querySelector('.js-showReport').classList.add('is-hidden');
     document.querySelector('.js-success-message-update').classList.add('is-hidden');
     document.querySelector('.js-success-message').classList.add('is-hidden');
 
 
 }
-function doReport(){
+
+function doReport() {
     document.querySelector('.js-showupdate').classList.add('is-hidden');
     document.querySelector('.js-showupadteCompany').classList.add('is-hidden');
     document.querySelector('.js-showReport').classList.remove('is-hidden');
     document.querySelector('.js-success-message-update').classList.add('is-hidden');
     document.querySelector('.js-success-message').classList.add('is-hidden');
-    
+    getTime();
+
 }
 
+
+function getTime() {
+    $("#getTime_").html('');
+    var worksheets = [
+        'Sheet4', // defaults to first worksheet without id
+        '4'
+    ];
+
+    worksheets.forEach(function (worksheet) {
+        $.googleSheetToJSON('1XjLtjloEkXKTDAsDLanA6Ace9ujINXbw7dClFAn5yeA', worksheet)
+            .done(function (rows) {
+                var strText = "<table border=1>";
+                var strText = "<table class='dtable'>";
+                strText += "<tr> <th>Ngày bắt đầu</th> <th>Số tuần</th>";
+                var count = 0;
+                rows.forEach(function (row) {
+                    count++;
+                    strText += "<tr>";
+                    Object.getOwnPropertyNames(row).forEach(function (name) {
+                        var val = [].concat(row[name]).join(' / ');
+                        strText += "<td>" + "<input style='border: none' type='text' readonly value='"+ val +"' />" + "</td>";
+
+                    });
+                    strText += "</tr>";
+
+                    return;
+                });
+                if (count == 0) {
+                    $("#getTime_").html('Lỗi');
+                } else {
+                    $("#getTime_").html(strText);
+                }
+            })
+            .fail(function (err) {
+                console.log('error!', err);
+            });
+    });
+}
 
 
