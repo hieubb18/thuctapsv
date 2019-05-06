8ee5bfb6a5249748bc947567c97fd5106d93e5bd
unknown <hieubb18>
2019-05-06 12:08:32 +0700

    Update do_get_info.js

--- do_get_info.js
+++ do_get_info.js
@@ -1,34 +1,26 @@
-// Get the input field
 var input = document.getElementById("inMaSV");
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("btnDoSV").click();
-    }
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("btnDoSV").click();
+}
 });
-
 function stepGetSinhVien() {
     var masv = $.trim($("input[name='txtMaSV']").val());
-    if (masv == '') {
+    if(masv == ''){
         alert("VUI LÒNG NHẬP MÃ SINH VIÊN");
         return false;
     }
     doLoading()
-        .then(doGetSinhVien)
-        .then(doComplete);
+    .then(doGetSinhVien)
+    .then(doComplete);
 }
-
 function doLoading() {
     return new Promise(function (resolve, reject) {
         document.querySelector('.js-loading').classList.remove('is-hidden');
         resolve();
     });
 }
-
 function doGetSinhVien() {
     return new Promise(function (resolve, reject) {
         sinhVienGet()
@@ -40,174 +32,253 @@ function doComplete() {
     return new Promise(function (resolve, reject) {
         setTimeout(() => {
             document.querySelector('.js-loading').classList.add('is-hidden');
-        }, 1000);
+        },1000);
         resolve();
     });
 }
 
+function openUpdateThongTin() {
+    // body...
+    document.querySelector('.js-showupdate').classList.remove('is-hidden');
+    document.querySelector('.js-showReport').classList.add('is-hidden');
+    document.querySelector('.js-success-message').classList.add('is-hidden');
+}
+
+function openBaoCaoThucTap() {
+    // body...
+    document.querySelector('.js-showReport').classList.remove('is-hidden');
+    document.querySelector('.js-showupdate').classList.add('is-hidden');
+    document.querySelector('.js-success-message').classList.add('is-hidden');
+    
+}
+
 function sinhVienGet() {
     $("#InfoSV").html('');
-
-    var masv = $.trim($("input[name='txtMaSV']").val()).replace(/ /g, '');
-
+    var masv = $.trim($("input[name='txtMaSV']").val()).replace(/ /g,'');
     var worksheets = [
-        '', // defaults to first worksheet without id
-        'ouab0ad'
-    ];
+        '',
+        'ouab0ad'];
 
-    worksheets.forEach(function (worksheet) {
-        $.googleSheetToJSON('1XjLtjloEkXKTDAsDLanA6Ace9ujINXbw7dClFAn5yeA', worksheet)
+        worksheets.forEach(function (worksheet) {
+            $.googleSheetToJSON('1nO2nV65Vi3dZWGlaIOXLEc-_JWEZK16XFbjQVH_3Q0U', worksheet)
             .done(function (rows) {
                 var strText = "<table border=1>";
                 var strText = "<table class='dtable'>";
-                strText += "<tr> <th>SĐT Giảng Viên</th>  <th>Email GV</th>  <th>Tên GV</th>  <th>Tên SV</th>  <th>Lớp</th> <th>Mã SV</th>  <th>Ngành</th>  <th>Ngày sinh</th>  <th>Email SV</th>  <th>Số ĐT </th>  <th>Môn Học</th>";
+                strText += "<tr> <th>SĐT Giảng Viên</th>  <th>Email GV</th>  <th>Tên GV</th>  <th>Tên SV</th>  <th>Lớp</th> <th>Mã SV</th>  <th>Ngành</th>  <th>Ngày sinh</th>  <th>Email SV</th>  <th>Số ĐT </th>  <th>Môn Học</th> ";
                 var count = 0;
                 rows.forEach(function (row) {
-                    var strMaSV = row['masv'].replace(/ /g, '');
+                    var strMaSV = row['masv'];
                     if (strMaSV == masv) {
                         count++;
                         strText += "<tr>";
                         Object.getOwnPropertyNames(row).forEach(function (name) {
-                            if (name == 'sotc' || name == 'tt' || name == 'mand' || name == 'mamh' || name == 'nhom' || name == 'tuan1-ndbaocao')
-                                return;
                             if (name == 'sv-email')
                                 $("input[name=EMAIL]").val(row[name]);
                             if (name == 'sv-sdt')
                                 $("input[name=DIENTHOAI]").val(row[name]);
+
+                            $("input[name="+name.toUpperCase()+"]").val(row[name]);
+
+                            if (name == 'sotc' || name == 'tt' || name == 'mand' || name == 'mamh' || name == 'nhom' || name ==='congty' || name === 'diachi' || name === 'ngaybatdau' || name === 'dienthoaiquanly' || name ==='chucvu' || name === 'vitricongviec' || name.match(/tuan.*/))
+                                return;                           
                             var val = [].concat(row[name]).join(' / ');
                             strText += "<td>" + val + "</td>";
                         });
                         strText += "</tr>";
-                        strText += "<div class='tools-sv'><div class='box-sv'><div class='box-sv-responsive'><button class='btn_' onclick='doupdateInfo();'>Cập nhật thông tin</button></div>";
-                        strText += "<div class='box-sv-responsive'><button class='btn_' onclick='doReport();'>Báo cáo</button></div><div class='clear'></div></div></div>";
-
-
                     }
                     return;
                 });
-                if (count == 0) {
+                if (count == 0)
+                {
                     $("#InfoSV").html('Không tìm thấy thông tin sinh viên');
                     document.querySelector('.js-showupdate').classList.add('is-hidden');
-                } else {
+                }
+                else {
                     $("#InfoSV").html(strText);
                     $("input[name=MASV]").val(masv);
-                    //document.querySelector('.js-showupdate').classList.remove('is-hidden');
+                    $("input[name=MASVREPORT]").val(masv);
+                    document.querySelector('.js-showuAction').classList.remove('is-hidden');
+                    getTime();
                 }
             })
             .fail(function (err) {
-                console.log('error!', err);
+                //
             });
-    });
-}
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbybJxIH0Q7NeJyxyY6HOznOUCMJ1IGPd5-l3SAojmvDLAMGPS3h/exec';
-const form = document.forms['submit-info-company'];
-const loading = document.querySelector('.js-loading');
-const successMessage = document.querySelector('.js-success-message-update');
-const errorMessage = document.querySelector('.js-error-message-update');
-const id_getInfo_company = document.querySelector('.js-showupadteCompany');
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    var value_input = $("input[name=MASV]").val().length;
-    if (value_input == 0) {
-        alert('BẠN CẦN NHẬP MÃ SV TRA CỨU THÔNG TIN TRƯỚC KHI THỰC HIỆN THAO TÁC NÀY');
-        return false;
+        });
     }
-    showLoadingIndicator();
-    fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form)
-        })
+
+    //form cap nhat thong tin sinh vien
+    const scriptURLCapNhatThongTin = 'https://script.google.com/macros/s/AKfycby_UEQn0LNrCEMAp1z1EoWGqCvfJvn8i9qVX9s2vsgdlZY_Me4/exec';
+    const formCapNhatThongTin = document.forms['submit-to-google-sheet'];
+    //form cap nhat bao cao thuc tap
+    const scriptURLBaoCao = 'https://script.google.com/macros/s/AKfycbzgL12d13EvS7Hz-zjgu4BSmQ6ALMKJb0dej-uwpqf6EA50GAE/exec';
+    const formBaoCao = document.forms['submit-form-report'];
+    
+    const loading = document.querySelector('.js-loading');
+    const successMessage = document.querySelector('.js-success-message');
+    const errorMessage = document.querySelector('.js-error-message');
+
+    formCapNhatThongTin.addEventListener('submit', e => {
+        e.preventDefault();
+        if ($("input[name=MASV]").val() == '') {
+            alert('BẠN CẦN NHẬP MÃ SV TRA CỨU THÔNG TIN TRƯỚC KHI THỰC HIỆN THAO TÁC NÀY');
+            return false;
+        }
+
+        showLoadingIndicatorFormCapNhatThongTin();
+        fetch(scriptURLCapNhatThongTin, { method: 'POST', body: new FormData(formCapNhatThongTin) })
         .then(response => showSuccessMessage(response))
         .catch(error => showErrorMessage(error));
-})
+    })
 
-function showLoadingIndicator() {
-    form.classList.add('is-hidden');
-    loading.classList.remove('is-hidden');
-}
+    formBaoCao.addEventListener('submit', e => {
+        e.preventDefault();
+        if ($("input[name=MASVREPORT]").val() == '') {
+            alert('BẠN CẦN NHẬP MÃ SV TRA CỨU THÔNG TIN TRƯỚC KHI THỰC HIỆN THAO TÁC NÀY');
+            return false;
+        }
 
-function showSuccessMessage(response) {
-    console.log('Success!', response);
-    setTimeout(() => {
-        successMessage.classList.remove('is-hidden');
-        loading.classList.add('is-hidden');
-        id_getInfo_company.classList.add('is-hidden');
-    }, 1000);
-    setTimeout(() => {
-        successMessage.classList.add('is-hidden');
+        showLoadingIndicatorBaoCao();
+        fetch(scriptURLBaoCao, { method: 'POST', body: new FormData(formBaoCao) })
+        .then(response => showSuccessMessage(response))
+        .catch(error => showErrorMessage(error));
+    })
 
-    }, 5000)
-}
+    function showLoadingIndicatorFormCapNhatThongTin() {
+        formCapNhatThongTin.classList.add('is-hidden');
+        loading.classList.remove('is-hidden');
+    }
 
-function showErrorMessage(error) {
-    console.error('Error!', error.message);
-    setTimeout(() => {
-        errorMessage.classList.remove('is-hidden');
-        loading.classList.add('is-hidden');
-    }, 1000);
-}
+    function showLoadingIndicatorBaoCao() {
+        formBaoCao.classList.add('is-hidden');
+        loading.classList.remove('is-hidden');
+    }
 
+    function showSuccessMessage(response) {
+        console.log('Success!', response);
+        setTimeout(() => {
+            successMessage.classList.remove('is-hidden');
+            loading.classList.add('is-hidden');
+        },1000);
+    }
 
-function doupdateInfo() {
-
-    document.querySelector('.js-showupdate').classList.remove('is-hidden');
-    document.querySelector('.js-showupadteCompany').classList.remove('is-hidden');
-    document.querySelector('.js-showReport').classList.add('is-hidden');
-    document.querySelector('.js-success-message-update').classList.add('is-hidden');
-    document.querySelector('.js-success-message').classList.add('is-hidden');
-
-
-}
-
-function doReport() {
-    document.querySelector('.js-showupdate').classList.add('is-hidden');
-    document.querySelector('.js-showupadteCompany').classList.add('is-hidden');
-    document.querySelector('.js-showReport').classList.remove('is-hidden');
-    document.querySelector('.js-success-message-update').classList.add('is-hidden');
-    document.querySelector('.js-success-message').classList.add('is-hidden');
-    getTime();
-
-}
+    function showErrorMessage(error) {
+        console.error('Error!', error.message);
+        setTimeout(() => {
+            errorMessage.classList.remove('is-hidden');
+            loading.classList.add('is-hidden');
+        },1000);
+    }
 
 
-function getTime() {
-    $("#getTime_").html('');
-    var worksheets = [
-        'Sheet4', // defaults to first worksheet without id
-        '4'
-    ];
+    function getTime() {
+        $("#getTime_").html('');
+        var worksheets = [
+        'Sheet3',
+        '3'
+        ];
 
-    worksheets.forEach(function (worksheet) {
-        $.googleSheetToJSON('1XjLtjloEkXKTDAsDLanA6Ace9ujINXbw7dClFAn5yeA', worksheet)
+        worksheets.forEach(function (worksheet) {
+            $.googleSheetToJSON('1nO2nV65Vi3dZWGlaIOXLEc-_JWEZK16XFbjQVH_3Q0U', worksheet)
             .done(function (rows) {
-                var strText = "<table border=1>";
-                var strText = "<table class='dtable'>";
-                strText += "<tr> <th>Ngày bắt đầu</th> <th>Số tuần</th>";
+                var ngayBatDau, soTuan, um4y, IDTuan;
                 var count = 0;
                 rows.forEach(function (row) {
                     count++;
-                    strText += "<tr>";
                     Object.getOwnPropertyNames(row).forEach(function (name) {
-                        var val = [].concat(row[name]).join(' / ');
-                        strText += "<td>" + "<input style='border: none' type='text' readonly value='"+ val +"' />" + "</td>";
-
+                        if(name == 'ngaybatdau')
+                            ngayBatDau = row[name];
+                        else if(name == 'sotuanthuchien')
+                            soTuan = row[name];
+                        else if(name == 'um4y')
+                            um4y = row[name];
+                        else if(name == 'idtuan')
+                            IDTuan = row[name];
                     });
-                    strText += "</tr>";
-
                     return;
                 });
                 if (count == 0) {
                     $("#getTime_").html('Lỗi');
                 } else {
-                    $("#getTime_").html(strText);
+                    addcontent(ngayBatDau, soTuan, um4y, IDTuan);
                 }
             })
             .fail(function (err) {
-                console.log('error!', err);
+                //
             });
-    });
+        });
+    }
+
+
+    function addcontent(ngayBatDau, soTuan, um4y, IDTuan){
+        var thoiGianBatDau, thoiGianKetThuc;
+        var thongTinTuan;
+        var danhSachTuan = [];
+        var parts = ngayBatDau.split('/');
+        var ngayBatDau = new Date(parts[2], parts[1] - 1, parts[0]); 
+        ngayBatDau = new Date(ngayBatDau.setDate(ngayBatDau.getDate()  - 7));
+        var isBaoCao = false;
+        for (var i = 0; i < soTuan; i++) {
+            thongTinTuan = [];
+            thongTinTuan.push("Tuần " + (i + 1));
+            thoiGianBatDau = new Date(ngayBatDau.setDate(ngayBatDau.getDate() + 7));
+            thongTinTuan.push("Từ "+thoiGianBatDau.toLocaleDateString('vi-VN'));
+            var temBD = new Date(thoiGianBatDau);
+            thoiGianKetThuc = new Date(thoiGianBatDau.setDate(thoiGianBatDau.getDate() + 6));
+            thongTinTuan.push("Đến "+thoiGianKetThuc.toLocaleDateString('vi-VN'));
+            strButton = "CHƯA TỚI THỜI GIAN BÁO CÁO HOẶC ĐÃ QUÁ HẠN BÁO CÁO";
+            if(!isBaoCao)
+            {
+                var partsTD = um4y.split('/');
+                var today = new Date(partsTD[2], partsTD[1] - 1, partsTD[0]);          
+                var strButton;
+                if(today >= temBD && today <= thoiGianKetThuc)
+                {
+                    strButton = "<button onclick='baocaotuan()' class='report_' >Báo cáo</button>";
+                    $("#form-report").append("<input type='hidden' name='TUANBAOCAO' value="+IDTuan+" >");
+                    $("input[name=MSSVTUAN]").val($("input[name=MASVREPORT]").val()+"-"+IDTuan);
+                    isBaoCao = true;
+                }
+            }
+            thongTinTuan.push(strButton);
+            danhSachTuan.push(thongTinTuan);
+        }
+        table = document.getElementById("time-report-tuan");
+        table.innerHTML = "";
+        for(var i = 0; i < danhSachTuan.length; i++)
+        {
+          var newRow = table.insertRow(table.length);
+          for(var j = 0; j < danhSachTuan[i].length; j++)
+          {
+              var cell = newRow.insertCell(j);
+              cell.innerHTML = danhSachTuan[i][j];
+          }
+      }
+  }
+
+  function baocaotuan(){
+    document.querySelector('.js-showNavReports').classList.remove('is-hidden');
+    document.querySelector('.js-success-message').classList.add('is-hidden');    
 }
 
+function resizeTextarea (id) {
+  var a = document.getElementById(id);
+  a.style.height = '110px';
+  a.style.height = a.scrollHeight+'px';
+}
 
+function init() {
+  var a = document.getElementsByTagName('textarea');
+  for(var i=0,inb=a.length;i<inb;i++) {
+   if(a[i].getAttribute('data-resizable')=='true')
+      resizeTextarea(a[i].id);
+}
+}
+
+addEventListener('DOMContentLoaded', init);
+
+
+//Clear contents
+function clearContents(element) {
+  element.value = '';
+}
